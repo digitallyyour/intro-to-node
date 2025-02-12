826ad1ee82d29848f1b873ee265a649c13174d17
Sushant Jain <digitallyyour@gmail.com>
2025-02-12 15:11:54 +0530

    added package.json

--- herokuDemo/heroku.js
+++ herokuDemo/heroku.js
@@ -16,7 +16,7 @@ http.createServer(function(req, res){
     fs.readFile(fileName, function(err, data){
         if (err) {
             res.writeHead(404, {'Content-Type':'text/html'});
-            return res.end("404 Not Found");
+            return res.end("404 Not Found....");
         }
         res.writeHead(200, {'Content-Type':'text/html'});
         res.write(data);
